f1d6ce165951f59bfefea10b3b11eb0c595cd1f8
Adarsh-311 <adarshpandit311@gmail.com>
2024-11-13 01:45:03 +0000

    datatypes

---
+++ basic_02.js/datatypes_01.js
@@ -0,0 +1,11 @@
+"use strict" // treat all js code asa newer version
+let name = "Adarsh" // String dataTypes
+let age = 3 // Number DataTypes
+let isLoggened = true //Boolean dataTypes
+// null=> standalone value
+// undefined => value is not assigned yet
+
+console.log(typeof "adarsh")
+console.log(typeof age)
+console.log(typeof null)
+console.table([typeof age,typeof null,typeof "Addarsh"])
