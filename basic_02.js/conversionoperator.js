c85c1448bb887eac8996e5f183f2d7e0f5f1efa0
Adarsh-311 <adarshpandit311@gmail.com>
2024-11-15 16:00:15 +0000

    operation

---
+++ basic_02.js/conversionoperator.js
@@ -0,0 +1,45 @@
+let score = "10"
+let score1 = "10abc"
+let n1 = null
+let n2 = undefined
+let n3 = true
+let n4 = "Adarsh"
+
+console.log(typeof(score))
+console.log(typeof score)
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+let valueInNumber3 = Number(n1)
+let valueInNumber4= Number(n2)
+let valueInNumber5= Number(n3)
+let valueInNumber6= Number(n4)
+console.log(valueInNumber)
+console.log(valueInNumber1)
+console.log(valueInNumber3)
+console.log(valueInNumber4)
+console.log(valueInNumber5)
+console.log(valueInNumber6)
+
+let isLoggenedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggenedIn)
+console.log(booleanIsLoggedIn)
+
+let a = 0
+let bool = Boolean(a)
+console.log(bool)
+
+let b =""
+let bo = Boolean(b)
+console.log(bo)
+
+
+let c ="Adarsh"
+let bul = Boolean(c)
+console.log(bul)
+
+
+let someString = 33
+let stringNumber = String(someString)
+console.log(stringNumber)
+console.log(typeof stringNumber)
---
+++ basic_02.js/operation.js
@@ -0,0 +1,28 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2*2)
+console.log(2**4)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "Adarsh"
+let str2 = " Pandit"
+let str3 = str1+str2
+console.log(str3);
+console.log("1" + 2)
+console.log("1" + "2")
+console.log("1"+ "2" +3)
+console.log(1 + 2 +"3")
+console.log((3+3)*(10%9)/(2/4))
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+let counter = 100
+counter++;
+// ++counter;
+console.log(counter)
