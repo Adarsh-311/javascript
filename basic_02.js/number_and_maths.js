030bc86dc79cbec6c2023e3376a1434c40fbfd9a
Adarsh-311 <adarshpandit311@gmail.com>
2024-11-18 23:33:17 +0000

    no&math

--- basic_02.js/number_and_maths.js
+++ basic_02.js/number_and_maths.js
@@ -1,28 +1,28 @@
-// const score = 100
-// console.log(score);
+const score = 100
+console.log(score);
 
-// const score1 = new Number(200)
-// console.log(score1);
+const score1 = new Number(200)
+console.log(score1);
 
-// console.log(score1.toString().length);
+console.log(score1.toString().length);
 
-// console.log(score1.toFixed(2)); //toFixed(2) gives the value like 200.00 and same if fixed value is (1) it will gives 200.0 
+console.log(score1.toFixed(2)); //toFixed(2) gives the value like 200.00 and same if fixed value is (1) it will gives 200.0 
 
-// const otherNumber = 123.456789
+const otherNumber = 123.456789
 
-// console.log(otherNumber.toPrecision(4));
+console.log(otherNumber.toPrecision(4));
 
-// const rupees = 1000000
-// console.log(rupees.toLocaleString('en-IN'));
+const rupees = 1000000
+console.log(rupees.toLocaleString('en-IN'));
 
-//++++++++++++++++++++++++++++++  Math   ++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++  Math   ++++++++++++++++++++++++++++++++++++
 
-// console.log(Math);
+console.log(Math);
 
-// console.log(Math.abs(-10));
-// console.log(Math.round(4.6));
-// console.log(Math.floor(46.6));
-// console.log(Math.ceil(46.6));
+console.log(Math.abs(-10));
+console.log(Math.round(4.6));
+console.log(Math.floor(46.6));
+console.log(Math.ceil(46.6));
 
 console.log(Math.random());
 console.log((Math.random()*10)+1);
