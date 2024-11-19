fbb3b1f0b865b5a5bbeff45e90bcf2fd5f9a8db8
Adarsh-311 <adarshpandit311@gmail.com>
2024-11-19 22:27:07 +0000

    revision

---
+++ object_new.js/revesion_obj.js
@@ -0,0 +1,21 @@
+
+const mySym = Symbol("Hello");
+
+const adarsh ={
+    namee :"Adarsh",
+    age : 20,
+    [mySym] : "Adarsh",
+    "full name" : "Adarsh Pandit",
+    Email :"adarshpandit311@gamil.com",
+    Address : "Noida",
+    mobNo:8541026679
+};
+Object.freeze(adarsh);
+adarsh.Email = "adarshxyz@gmail.com"
+console.log(adarsh);
+
+adarsh.fun1 = function(){
+    console.log(`How Are You ${this.namee}`);
+}
+// console.log(adarsh.fun1());
+
