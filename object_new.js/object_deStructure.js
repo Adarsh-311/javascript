b1f912dfce809a9dec42e91c87e22913ee9531bc
Adarsh-311 <adarshpandit311@gmail.com>
2024-11-21 22:01:19 +0000

    deStructure of object

---
+++ object_new.js/object_deStructure.js
@@ -0,0 +1,10 @@
+const course ={
+    coursename : "js",
+    coursePrise : "999",
+    courseInstructore : "hitesh"
+}
+
+const {courseInstructore: instructore} = course
+
+
+console.log(instructore);
