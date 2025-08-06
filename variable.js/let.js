949ed07f9b2033a33dad355d3fad8a4716882b75
Adarsh Kumar <adarshpandit311@gmail.com>
2025-08-06 09:16:29 +0000

    variable(let)

---
+++ variable.js/let.js
@@ -0,0 +1,16 @@
+{
+    let x = 10
+    console.log(x);
+    
+}
+
+// console.log(x); throw error ReferenceError: x is not defined
+let x = 10;
+    if (true) {
+        let y = 20;
+        console.log(x);
+        console.log(y);
+    }
+    console.log(x);
+    // console.log(y);
+    
