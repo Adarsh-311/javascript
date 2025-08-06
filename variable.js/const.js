196c352665d4efaf3f8cb0b1dedcf9d340b2871c
Adarsh Kumar <adarshpandit311@gmail.com>
2025-08-06 09:23:42 +0000

    variable(const)

---
+++ variable.js/const.js
@@ -0,0 +1,12 @@
+const a = 10;
+console.log(a);
+
+// a = 20;
+// console.log(a);TypeError: Assignment to constant variable.
+
+
+const z = 30;
+console.log(z);
+const person = { name: "Adarsh" };
+person.name = "Rahul"; // ✅ Allowed
+console.log(person.name); // Rahul
