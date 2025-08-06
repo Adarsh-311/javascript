4dd23e0d3d74433b350446f0c4e2b7b40e68e051
Adarsh Kumar <adarshpandit311@gmail.com>
2025-08-06 09:07:39 +0000

    variable(var)

---
+++ variable.js/var01.js
@@ -0,0 +1,15 @@
+function ex(){
+    var x = 10;
+    if(true){
+        var x = 20;
+        console.log("Inside if block : ",x);
+    }
+    console.log("Outside If Block :",x);
+    
+}
+// console.log(x);
+ex();
+
+var x = 30;
+var x = 40;
+console.log(x);
